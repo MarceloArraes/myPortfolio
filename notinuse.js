49796ade9c5f9d5880b1ab7dc202c6145082557f
marcelo <marcelo.arraes@gmail.com>
2022-03-30 20:46:45 +0000

    feat: starting to learn tailwind fundamentals

---
+++ notinuse.js
@@ -0,0 +1,35 @@
+;<a
+  className="mt-6 w-96 basis-1/2 rounded-xl border text-left hover:text-blue-600 focus:text-blue-600"
+  href="https://adaminter.org/"
+>
+  <div className="h-fullrounded-lg relative h-96">
+    {/* Background Image  */}
+    <img
+      src="adaminterback.png"
+      className="h-full w-full rounded-lg object-cover"
+    />
+
+    {/* Content  */}
+    <div className="absolute bottom-0 flex h-full w-full flex-col items-center justify-center rounded-lg text-center backdrop-blur-sm">
+      {/* <!-- Company Logo --> */}
+      {/* <img src="" /> */}
+
+      {/* <!-- Quotes --> */}
+      <p className="mt-10 px-14 text-lg text-gray-800">
+        Adaminter is a site were any user can register a NFT on the Cardano
+        Blockchain paying a very low fee.
+      </p>
+
+      {/* <!-- Photo --> */}
+      <img
+        src="reacticon.png"
+        className="mt-8 w-12 animate-spin-slow rounded-full border-2 "
+      />
+
+      {/* <!-- Title --> */}
+      <p className="mt-3 px-14 text-base font-bold  text-gray-800 ">Reactjs</p>
+
+      <p className="px-14 text-sm font-light text-gray-800">Adaminter</p>
+    </div>
+  </div>
+</a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,75 +9,119 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+      <main className="py-10">
+        <div className='className="flex text-center" w-full flex-1 flex-col items-center justify-center px-20'>
+          <h1 className="text-6xl font-bold">
+            Welcome to{' '}
+            <a className="text-blue-600" href="https://nextjs.org">
+              Next.js!
+            </a>
+          </h1>
+          <p className="mt-3 text-2xl">
+            Get started by editing{' '}
+            <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
+              pages/index.tsx
+            </code>
+          </p>
         </div>
       </main>
+      <div className="flex flex-wrap items-center justify-center">
+        <div className="relative mx-2 mb-6 max-w-xs flex-shrink-0 overflow-hidden rounded-lg bg-yellow-500 shadow-lg">
+          <svg
+            className="absolute bottom-0 left-0 mb-8"
+            viewBox="0 0 375 283"
+            fill="none"
+          ></svg>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          <div className="relative flex items-center justify-center px-10 pt-10">
+            <div className="absolute bottom-0 left-0 -mb-24 ml-3 block h-48 w-48"></div>
+            <picture>
+              <source srcSet="/images/object/5.webp" type="image/webp" />
+              <source srcSet="/images/object/5.png" />
+              <img
+                className="relative w-40"
+                src="/images/object/5.png"
+                alt="shopping item"
+              />
+            </picture>
+          </div>
+          <div className="relative mt-6 px-6 pb-6 text-white">
+            <span className="-mb-1 block opacity-75">Indoor</span>
+            <div className="flex justify-between">
+              <span className="block text-xl font-semibold">Peace Lily</span>
+              <span className="flex items-center rounded-full bg-white px-3 py-2 text-xs font-bold leading-none text-yellow-500">
+                $36.00
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="relative mx-2 mb-6 max-w-xs flex-shrink-0 overflow-hidden rounded-lg bg-blue-500 shadow-lg ">
+          <svg
+            className="absolute bottom-0 left-0 mb-8"
+            viewBox="0 0 375 283"
+            fill="none"
+          ></svg>
+          <div className="relative flex items-center justify-center px-10 pt-10">
+            <div className="absolute bottom-0 left-0 -mb-24 ml-3 block h-48 w-48"></div>
+            <img
+              className="relative w-40"
+              src="/images/object/6.png"
+              alt="shopping"
+            />
+          </div>
+          <div className="relative mt-6 px-6 pb-6 text-white">
+            <span className="-mb-1 block opacity-75">Outdoor</span>
+            <div className="flex justify-between">
+              <span className="block text-xl font-semibold">Monstera</span>
+              <span className="flex items-center rounded-full bg-white px-3 py-2 text-xs font-bold leading-none text-blue-500">
+                $45.00
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className="relative mx-2 mb-6 max-w-xs flex-shrink-0 overflow-hidden rounded-lg bg-purple-500 shadow-lg">
+          <svg
+            className="absolute bottom-0 left-0 mb-8"
+            viewBox="0 0 375 283"
+            fill="none"
+          ></svg>
+          <div className="relative flex items-center justify-center px-10 pt-10">
+            <div className="absolute bottom-0 left-0 -mb-24 ml-3 block h-48 w-48"></div>
+            <img
+              className="relative w-40"
+              src="/images/object/7.png"
+              alt="shopping"
+            />
+          </div>
+          <div className="relative mt-6 px-6 pb-6 text-white">
+            <span className="-mb-1 block opacity-75">Outdoor</span>
+            <div className="flex justify-between">
+              <span className="block text-xl font-semibold">Oak Tree</span>
+              <span className="flex items-center rounded-full bg-white px-3 py-2 text-xs font-bold leading-none text-purple-500">
+                $68.50
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <footer className="flex h-24 w-full items-center justify-center border-t ">
+        <p className="flex items-center justify-center gap-2">
           Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
+          <Image
+            src="/Doom-2-Cheats.png"
+            alt="Doom Guy Face"
+            width={72}
+            height={56}
+          />
+          <Image
+            src="/doomguygif.gif"
+            alt="Doom Guy Face"
+            width={72}
+            height={56}
+          />
+          <Image src="/giphy.gif" alt="Doom Guy Face" width={72} height={56} />
+        </p>
       </footer>
     </div>
   )
