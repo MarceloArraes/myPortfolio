ecb537d5d7b8a0e442da1b23aba7296ed8b5a32f
marcelo <marcelo.arraes@gmail.com>
2022-04-16 01:35:42 +0000

    improv: add another style of button to analize

--- components/title.tsx
+++ components/title.tsx
@@ -28,7 +28,25 @@ function Title() {
               download="Curriculum_Marcelo_Arraes"
             >
               <button
-                className="flex h-12 w-36 items-center justify-center rounded-lg bg-white p-3 leading-tight text-black transition duration-150 ease-in-out hover:shadow-lg focus:bg-blue-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+                className="
+                flex
+                h-12 
+                w-36
+                items-center
+                justify-center 
+                rounded-lg
+                bg-white
+                p-3 
+                leading-tight
+                text-black
+                transition
+                duration-150
+                ease-in-out 
+                hover:shadow-lg
+                focus:shadow-lg
+                focus:outline-none
+                focus:ring-0
+                active:shadow-lg"
                 type="button"
               >
                 <svg
@@ -51,13 +69,97 @@ function Title() {
               </button>
             </a>
             <a
+              href="Curriculo Marcelo Arraes Teixeira.pdf"
+              download="Curriculum_Marcelo_Arraes"
+            >
+              <button
+                className="
+              flex
+              items-center
+              justify-center 
+              rounded-full
+              border-2 
+              border-gray-500
+              px-6
+              py-2
+              uppercase
+              transition
+              duration-200
+              ease-in
+              hover:bg-gray-700
+              hover:text-white 
+              hover:shadow-lg
+              focus:outline-none"
+              >
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="download"
+                  className="mt-1 mr-2 w-3 "
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+                  ></path>
+                </svg>
+                CV pt-br
+                <span className="absolute h-12 w-36 rounded-lg"></span>
+              </button>
+            </a>
+
+            <a
+              href="Curriculum - en - Marcelo Arraes Teixeira.pdf"
+              download="Resume_en_Marcelo_Arraes"
+            >
+              <button
+                className="
+              flex 
+              items-center
+              justify-center
+              rounded-full
+              border-2 
+              border-gray-500
+              px-6 
+              py-2
+              uppercase 
+              transition
+              duration-200
+              ease-in
+              hover:bg-gray-800
+              hover:text-white
+              focus:outline-none"
+              >
+                <svg
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fas"
+                  data-icon="download"
+                  className=" mr-2 w-3"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M216 0h80c13.3 0 24 10.7 24 24v168h87.7c17.8 0 26.7 21.5 14.1 34.1L269.7 378.3c-7.5 7.5-19.8 7.5-27.3 0L90.1 226.1c-12.6-12.6-3.7-34.1 14.1-34.1H192V24c0-13.3 10.7-24 24-24zm296 376v112c0 13.3-10.7 24-24 24H24c-13.3 0-24-10.7-24-24V376c0-13.3 10.7-24 24-24h146.7l49 49c20.1 20.1 52.5 20.1 72.6 0l49-49H488c13.3 0 24 10.7 24 24zm-124 88c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20zm64 0c0-11-9-20-20-20s-20 9-20 20 9 20 20 20 20-9 20-20z"
+                  ></path>
+                </svg>
+                CV en-us
+              </button>
+            </a>
+
+            <a
               href="Curriculum - en - Marcelo Arraes Teixeira.pdf"
               download="Curriculum_en_Marcelo_Arraes"
             >
               <button
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
-                className="flex h-12 w-36 items-center justify-center rounded-lg bg-white p-3 leading-tight text-black transition duration-150 ease-in-out hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+                className="flex h-12 w-36 items-center justify-center rounded-lg bg-white p-3 leading-tight text-black transition duration-150 ease-in-out hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg"
                 type="button"
               >
                 <svg
